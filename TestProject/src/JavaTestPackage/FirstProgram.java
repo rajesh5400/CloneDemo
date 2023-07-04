e77f3a810dc0be5b15787308db9a1cd03c9d2d54
Rajesh <Rajesh@C40LAP-3271.india.rsystems.com>
2023-07-04 23:32:17 +0530

    Commit to CloneBranch

---
+++ TestProject/src/JavaTestPackage/FirstProgram.java
@@ -0,0 +1,15 @@
+package JavaTestPackage;
+
+import org.openqa.selenium.WebDriver;
+
+public class FirstProgram {
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		System.out.println("Heloo world");
+WebDriver driver= new Webdriver(); 
+
+System.out.println("Demo");
+	}
+
+}
